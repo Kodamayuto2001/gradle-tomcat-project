5ebe6e2713872fadb33eac92341b5e3b1d8af79e
Kodamayuto2001 <yuto2001gyokugan@gmail.com>
2022-02-02 22:52:53 +0900

    やっと環境構築が終わった（CI/CD以外）、ホンマに感動したわ

---
+++ src/main/java/gradle/sample/SampleServlet.java
@@ -0,0 +1,17 @@
+package gradle.sample;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class SampleServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/plain;charset=utf-8");
+		response.getWriter().print("Hello World!");
+	}
+}
